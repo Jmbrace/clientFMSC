df153682fe609c558fee60efca34c8336119e8df
Jmbrace <jmbrace@ilstu.edu>
2016-04-25 16:48:29 -0500

    Grunt file update

--- Gruntfile.js
+++ Gruntfile.js
@@ -328,7 +328,7 @@ module.exports = function (grunt) {
         },
         files: [{
           expand: true,
-          cwd: '<%= yeoman.dist %>',
+          cwd: '<%= yeoman.app %>',
           src: ['*.html'],
           dest: '<%= yeoman.dist %>'
         }]
@@ -343,7 +343,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/{,*/}*.html',
+        src: 'scripts/{,**}*.html',
         dest: '.tmp/templateCache.js'
       }
     },
