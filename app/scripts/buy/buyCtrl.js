5c679b22834eecbbe9926c378175c2d3786a907c
jared engler <jared.lite@gmail.com>
2016-04-27 20:24:05 -0500

    updated, buy, contact, main, and login

--- app/scripts/buy/buyCtrl.js
+++ app/scripts/buy/buyCtrl.js
@@ -8,17 +8,40 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('buyCtrl', ['apiService', '$scope', function (apiService, $scope) {
+  .controller('buyCtrl', ['apiService', '$scope', 'paragraph', function (apiService, $scope, paragraph) {
+
+    // init bound vars
+    $scope.name = "";
+    $scope.meals = 20;
+    $scope.state = "";
+    $scope.message = "";
+    $scope.badWord = false;
+
+    $scope.nsfwMessage =  function() {
+        if (paragraph.isGentle($scope.message).length > 0) {
+            $scope.badWord=true;
+        }
+        else {
+            $scope.badWord=false;
+            console.log("In here");
+        }
+    }
 
     // Stripe Response Handler
     $scope.stripeCallback = function (code, result) {
         if (result.error) {
-            window.alert('it failed! error: ' + result.error.message);
+            window.alert(result.error.message);
         } else {
-            window.alert('success! token: ' + result.id);
+            window.alert(result.id);
 
+            var payload = {
+              //
+              "stripeToken": result.id,
+              "numOfMeals": $scope.meals
+            }
             //Make call to mark X pixels as 'in purchase', and set message
-            
+
         }
     };
+
   }]);
