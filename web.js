ea8006f4d72d486016fcef1e8232d610bb1865dd
Jmbrace <jmbrace@ilstu.edu>
2016-04-28 14:12:02 -0500

    client

--- web.js
+++ web.js
@@ -147,3 +147,7 @@
 		app.get('/scripts/profile/profile.html', function (req, res){
 	    res.sendfile('/app/scripts/profile/profile.html', {root: __dirname})
 	});
+
+		app.get('/images/avatar.png', function (req, res){
+	    res.sendfile('/app/images/avatar.png', {root: __dirname})
+	});
