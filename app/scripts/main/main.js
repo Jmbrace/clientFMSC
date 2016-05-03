5a8963508ce15cac21b56ece0f343d12d8311d23
alectroemel <alectroemel@hotmail.com>
2016-05-03 17:04:44 -0500

    states working

--- app/scripts/main/main.js
+++ app/scripts/main/main.js
@@ -71,35 +71,97 @@ angular.module('clientApp')
 
 
 	var imageDiv = $("#imageDiv");
-	console.log(imageDiv.width() + imageDiv.height() );
 	var scale = imageDiv.width() / 4000;
-	console.log("scale: " + scale )
-
 	var messages = [];
+	var jsonResponse; 
 
 	function setUpMessages() {
 		$http.get("https://fmsc-server.herokuapp.com/api/blocks/1").then(function(response) {
 			
 			console.log(response);
 
-			var obj = response.data;//JSON.parse(response);
+			jsonResponse = response.data;//JSON.parse(response);
 
-	 		for (var i = 0; i < obj.blocks.length; i++) { 
-	 			console.log(obj.blocks[i]);
+			// set up states 
+	        var states = {
+		        "Alabama":0,
+		        "Alaska":0,
+		        "Arizona":0,
+		        "Arkansas":0,
+		        "California":0,
+		        "Colorado":0,
+		        "Connecticut":0,
+		        "Delaware":0,
+		        "Florida":0,
+		        "Georgia":0,
+		        "Hawaii":0,
+		        "Idaho":0,
+		        "Illinois":0,
+		        "Indiana":0,
+		        "Iowa":0,
+		        "Kansas":0,
+		        "Kentucky":0,
+		        "Louisiana":0,
+		        "Maine":0,
+		        "Maryland":0,
+		        "Massachusetts":0,
+		        "Michigan":0,
+		        "Minnesota":0,
+		        "Mississippi":0,
+		        "Missouri":0,
+		        "Montana":0,
+		        "Nebraska":0,
+		        "Nevada":0,
+		        "New Hampshire":0,
+		        "New Jersey":0,
+		        "New Mexico":0,
+		        "New York":0,
+		        "North Carolina":0,
+		        "North Dakota":0,
+		        "Ohio":0,
+		        "Oklahoma":0,
+		        "Oregon":0,
+		        "Pennsylvania":0,
+		        "Rhode Island":0,
+		        "South Carolina":0,
+		        "South Dakota":0,
+		        "Tennessee":0,
+		        "Texas":0,
+		        "Utah":0,
+		        "Vermont":0,
+		        "Virginia":0,
+		        "Washington":0,
+		        "West Virginia":0,
+		        "Wisconsin":0,
+		        "Wyoming":0,
+		        "No State":0
+			}
+			var count = 0;
 
+	 		for (var i = 0; i < jsonResponse.blocks.length; i++) { 
+	 			console.log(jsonResponse.blocks[i]);
+	 			if (jsonResponse.blocks[i].state == "") {
+	 				states["No State"] += 1;
+	 			}
+	 			else {
+	 				states[jsonResponse.blocks[i].state] += 1;
+	 			}
+	 			count += 1;
 	 			var message = {
-	 				origX: obj.blocks[i].leftXCrnr,
-	 				origY: obj.blocks[i].leftYCrnr,
-				    x1: Math.round(obj.blocks[i].leftXCrnr * scale),
-				    y1: Math.round(obj.blocks[i].leftYCrnr * scale),
-				    x2: Math.round((obj.blocks[i].leftXCrnr + 16) * scale), 
-				    y2: Math.round((obj.blocks[i].leftYCrnr + 15) * scale),  
-				    message: obj.blocks[i].message
+	 				origX: jsonResponse.blocks[i].leftXCrnr,
+	 				origY: jsonResponse.blocks[i].leftYCrnr,
+				    x1: Math.round(jsonResponse.blocks[i].leftXCrnr * scale),
+				    y1: Math.round(jsonResponse.blocks[i].leftYCrnr * scale),
+				    x2: Math.round((jsonResponse.blocks[i].leftXCrnr + 16) * scale), 
+				    y2: Math.round((jsonResponse.blocks[i].leftYCrnr + 15) * scale),  
+				    message: jsonResponse.blocks[i].message
 				};
 
 				messages.push(message);
 			}
-			console.log(messages);
+
+			intiStats(states, count);
+			//console.log(messages);
 		});	
 	}
 
@@ -115,29 +177,49 @@ angular.module('clientApp')
 	var top5Data;
 	var allStatesConfig;
 
-	function intiStats()
+	function intiStats(states, count)
 	{
+		console.log(count);
 		progressBarData = {
           labels: ["Progress"],
           datasets: [{
               label: 'Pixels Purchased',
               backgroundColor: "rgba(73,232,0,1)",
               highlightStroke: "black",
-              data: [25]
+              data: [count]
           }, {
               label: 'Pixels Left',
               backgroundColor: "rgba(232,43,0,1)",
-              data: [75]
+              data: [50000-count]
           }]
       	};
 
+        var temp = [];
+		for (var key in states) {
+		    temp.push([key, states[key]]);
+		}
+
+		temp.sort(function(a, b) {return b[1] - a[1]})
+		console.log(temp);
+
+
+		var values = []
+		for (var i = 0; i < 50; i++) {
+			values[i] = temp[i][1];
+		}
+		var stateNames = []
+		for (var i = 0; i < 50; i++) {
+			stateNames[i] = temp[i][0];
+		}
+		
+
       	top5Data = {
-            labels: ["State 1", "State 2", "State 3", "State 4", "State 5"],
+            labels: stateNames.slice(0,5),
             datasets: [{
                 label: 'Pixels',
                 backgroundColor: "blue",
                 highlightStroke: "black",
-                data: [12,44,33,55,77]
+                data: values.slice(0,5)
             }]
 
         };
@@ -146,8 +228,7 @@ angular.module('clientApp')
 	        type: 'doughnut',
 	        data: {
 	            datasets: [{
-	                data: [75,5,33,55,22,75,55,3,55,22,75,5,3,5,2,75,5,3,5,22,7,5,3,55,22,
-	                        7,5,3,55,2,75,5,33,55,22,75,55,33,55,22,75,55,33,55,22,75,55,33,55,22],
+	                data: values,
 	                backgroundColor: [
 	                    "#F7464A",
 	                    "#46BFBD",
@@ -202,58 +283,7 @@ angular.module('clientApp')
 	                ],
 	                label: 'Dataset 1'
 	            }],
-	            labels: [
-	                "Alabama",
-	                "Alaska",
-	                "Arizona",
-	                "Arkansas",
-	                "California",
-	                "Colorado",
-	                "Connecticut",
-	                "Delaware",
-	                "Florida",
-	                "Georgia",
-	                "Hawaii",
-	                "Idaho",
-	                "Illinois",
-	                "Indiana",
-	                "Iowa",
-	                "Kansas",
-	                "Kentucky",
-	                "Louisiana",
-	                "Maine",
-	                "Maryland",
-	                "Massachusetts",
-	                "Michigan",
-	                "Minnesota",
-	                "Mississippi",
-	                "Missouri",
-	                "Montana",
-	                "Nebraska",
-	                "Nevada",
-	                "New Hampshire",
-	                "New Jersey",
-	                "New Mexico",
-	                "New York",
-	                "North Carolina",
-	                "North Dakota",
-	                "Ohio",
-	                "Oklahoma",
-	                "Oregon",
-	                "Pennsylvania",
-	                "Rhode Island",
-	                "South Carolina",
-	                "South Dakota",
-	                "Tennessee",
-	                "Texas",
-	                "Utah",
-	                "Vermont",
-	                "Virginia",
-	                "Washington",
-	                "West Virginia",
-	                "Wisconsin",
-	                "Wyoming"
-	            ]
+	            labels: stateNames
 	        },
 	        options: {
 	            responsive: false,
@@ -332,6 +362,5 @@ angular.module('clientApp')
         var allStates = new Chart(ctx2, allStatesConfig);
 	}
 
-	intiStats();
     $rootScope.isLoggedIn = authService.loggedIn;
   }]);
